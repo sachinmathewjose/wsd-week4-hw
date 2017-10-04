8bcca86c58797c572ff7fd2db47f7dfbfc47bc49
sachin mathew jose <sachin.jose9@gmail.com>
2017-10-03 21:13:30 -0400

    corrected according to comments from professor

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $date =  date('Y-m-d', time());
-echo "The value of \$date: ".$date."<br>";
+$dateString = "The value of \$date: ".$date;
+echo $dateString."<br>";
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
@@ -9,22 +10,19 @@ $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year);
 
-echo "<br><hr>Start of homework: <br>";
-
-$obj = new main($date,$tar,$year);
-
+$obj = new main($date,$tar,$year,$dateString);
 
 class main
 {
-	
-	public function __construct($date, $tar, $year)
+	public function __construct($date, $tar, $year,$dateString)
 	{
+		echo "<br><hr>Start of homework: <br>";
 		$this-> question2($date);
 		$this-> question3($date, $tar);
 		$this-> question4($date);
-		$this-> question5($date);
-		$this-> question6($date);
-		$this-> question7($date);
+		$this-> question5($dateString);
+		$this-> question6($dateString);
+		$this-> question7($dateString);
 		$this-> question8($date);
 		$this-> question9($date);
 		$this-> question10($year);
@@ -33,7 +31,7 @@ class main
 	public function question2(&$date)
 	{
 		$date = str_replace("-", "/", $date);
-		echo "Q2. date replaced with \"/\": ".$date;
+		echo "Q2. date replaced with \"/\" : ".$date;
 	}
 
 	public function question3($date, $tar)
@@ -43,7 +41,8 @@ class main
 		{
 			echo "the future";
 		}
-		elseif (strcmp($date, $tar) < 0) {
+		elseif (strcmp($date, $tar) < 0)
+		{
 			echo "the past";
 		}
 		else
@@ -64,19 +63,19 @@ class main
 		}
 	}
 
-	public function question5($date)
+	public function question5($dateString)
 	{
-		echo "<br><br>Q5. The number of words in \"$date\" is: ".str_word_count($date);
+		echo "<br><br>Q5. The number of words in \"$dateString\" is: ".str_word_count($dateString);
 	}
 
-	public function question6($date)
+	public function question6($dateString)
 	{
-		echo "<br><br>Q6. length of the string is: ".strlen($date);
+		echo "<br><br>Q6. length of the string is: ".strlen($dateString);
 	}
 
-	public function question7($date)
+	public function question7($dateString)
 	{
-		echo "<br><br>Q7. ASCII value of the first vharacter is: ".ord($date);
+		echo "<br><br>Q7. ASCII value of the first character is: ".ord($dateString);
 	}
 
 	public function question8($date)
@@ -87,24 +86,48 @@ class main
 	public function question9($date)
 	{
 		echo "<br><br>Q9. The contents in the generated array is : ";
-		foreach (explode("/", $date) as $value) {
+		foreach (explode("/", $date) as $value)
+		{
 			echo "$value ";
 		}
 	}
 
 	public function question10($years)
 	{
-		echo "<br><br>Q10. Listing years to check if it is a leap year:";
+		echo "<br><br>Q10. Listing years to check if it is a leap year (Using switch and foreach):";
+		foreach ($years as $year)
+		{
+			echo "$year -> ";
+			switch ($year%4)
+			{
+				case 0:
+					switch ($year % 100)
+					{
+						case 0:
+							switch ($year % 400)
+							{
+								case 0: echo "True ";
+								break;
+								default: echo "False ";
+							}
+							break;
+						default: echo "True ";
+					}
+					break;
+				default: echo "False ";
+			}
+		}
+		echo "<br><br>Q10. Listing years to check if it is a leap year (Using for loop and if):";
 		foreach ($years as $year) {
 
-			echo "<br> $year  ";
-			if( ($year % 4 == 0) and ($year % 100 != 0) or ($year % 400 == 0) )
+			echo "$year -> ";
+			if( ($year % 4 == 0) && ($year % 100 != 0) || ($year % 400 == 0) )
 			{
-				echo "True";
+				echo "True ";
 			}
 			else
 			{
-				echo "False";
+				echo "False ";
 			}
 		}
 	}
